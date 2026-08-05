20c2ed2e3e3645626e9e2259b313119b2b3cb225
Marcello Prado <marcellopradomuller@gmail.com>
2026-08-05 20:13:49 -0300

    refactorando: reorganizar estrutura do projeto e iniciar backend com FastAPI

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title= "Pedido Cashback API",
+    version="1.0.0"
+)
+
+#^ Permite que o frontend acesse a API (Faça a requisição)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #TODO: DEPOIS RESTRINGIR PARA O DOMÍNIO DO FRONTEND
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {
+        "status": "online",
+        "mensagem": "API de pedidos de cashback está online!"
+    }
+    
